81c741b6cec6307a3225e3510aaa93c66483c4da
Ritik947 <84516324+Ritik947@users.noreply.github.com>
2021-06-20 21:29:47 +0530

    Day 9 THA

--- Day 9/script.js
+++ Day 9/script.js
@@ -1,12 +1,30 @@
-var box = document.querySelectorAll(".box");
-box.forEach(item => {
-    item.addEventListener('click', () => {
-        // if(box.classList.contains('boxclicked')){
-        //     box.classList.remove('boxclicked');
-        // }
-        // else{
-        //     box.classList.add('boxclicked');
-        // }
-        item.classList.toggle('boxclicked');
-    })
-})
+const container = document.querySelector('.container');
+var b = 0, r = 36;
+const booked = document.querySelector('.booked');
+const remaining = document.querySelector('.remaining');
+
+const book = () => {
+    booked.innerHTML = "Booked Seats: " + b;
+    remaining.innerHTML = "Remaining Seats: " + r;
+}
+
+book();
+
+for (let id = 1; id <= 36; id++){
+    let box = document.createElement('div');
+    box.setAttribute('class','box');
+    box.setAttribute('id', id);
+    box.addEventListener('click',() => {
+        if (!box.classList.contains('boxclicked')){
+            b+=1;
+            r-=1;
+        }
+        else{
+            b-=1;
+            r+=1;
+        }
+        box.classList.toggle('boxclicked');
+        book();    
+    });
+    container.appendChild(box);
+}
