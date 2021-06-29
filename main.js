d11bd5e19c09e8ac9b2826a447d2fc57c81b45da
Ritik947 <84516324+Ritik947@users.noreply.github.com>
2021-06-29 23:54:38 +0530

    Index done

---
+++ main.js
@@ -0,0 +1,22 @@
+const thaGridClass = document.querySelector("#tha-list");
+
+// const createHrefElement = (day_no) => {
+//   let anchor = document.createElement("a");
+//   anchor.href = "/Day " + day_no;
+//   anchor.innerHTML = "Day " + day_no;
+// };
+
+const day_nos = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+
+for (let i = 0; i < day_nos.length; i++) {
+  let pix = document.createElement("div");
+  let text = document.createElement("p");
+  text.innerHTML = "Day " + day_nos[i];
+  pix.setAttribute("class", "pix");
+  pix.setAttribute("id", i + 1);
+  pix.addEventListener("click", () => {
+    window.location.href = "/Front-End-Dev/Day " + day_nos[i];
+  });
+  pix.appendChild(text);
+  thaGridClass.appendChild(pix);
+}
