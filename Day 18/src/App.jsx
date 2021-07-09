45d36e876f7790e82c346ba75969df74e92a5215
Ritik947 <84516324+Ritik947@users.noreply.github.com>
2021-07-09 15:36:48 +0530

    Day 18 THA done

---
+++ Day 18/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Chessboard from "./components/Chessboard";
+
+function App() {
+  return (
+    <div className="App">
+      <header>ChessBoard</header>
+      <main>
+        <Chessboard />
+      </main>
+    </div>
+  );
+}
+
+export default App;
